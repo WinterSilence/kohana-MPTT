ee0cc38a19b976d45dd000624a9ef8a9ae70602d
Anton <antonfedonjuk@gmail.com>
2019-03-30 15:46:24 +0300

    Update reference.php

--- i18n/reference.php
+++ i18n/reference.php
@@ -1,7 +1,5 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array
-(
-    'after'           => '',
-    'first child of'  => '',
-);
+<?php
+return [
+    'after'          => 'after',
+    'first child of' => 'first child of',
+];
